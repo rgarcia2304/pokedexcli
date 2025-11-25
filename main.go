9d4bc4e71c3c6ffc673fdaded37f84e2a3a06888
rgarcia2304 <garcia2304@icloud.com>
2025-11-25 18:24:16 -0500

    Added a small command line interface

--- main.go
+++ main.go
@@ -4,10 +4,24 @@ package main
 import(
 
 "fmt"
+"bufio"
+"os"
 )
 
 
 func main(){
+	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("Hello, World!")
+	for{
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		scannerVal := scanner.Text()
+		//clean the scanned value
+		cleanedScan := cleanInput(scannerVal)
+
+		//capture first word of input 
+		term_msg := fmt.Sprintf("Your command was: %v", cleanedScan[0])
+		fmt.Println(term_msg)
+	}
+
 }
